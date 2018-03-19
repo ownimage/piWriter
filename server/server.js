7169bd9e198529de57de3b3f744f617f3eefcdf8
ownimage <keith@ownimage.co.uk>
2018-03-19 07:18:45 +0000

    Added server app.get('/v1/playlists/:playlist', getPlaylistV1);

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const fs = require('fs');
+const path = require('path');
 
 const playlistFolder = '../library/playlists';
 
@@ -12,6 +13,20 @@ const getPlaylistsV1 = (req, res) => {
     });
 };
 
+const getPlaylistV1 = (req, res) => {
+    console.log(req.params.playlist);
+    const playlist = req.params.playlist;
+    const filePath = path.join(playlistFolder, playlist);
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+        if (!err) {
+            console.log('received data: ' + data);
+            res.send(data);
+        } else {
+            console.log(err);
+        }
+    });
+}
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -20,6 +35,8 @@ app.get('/users/:userId/books/:bookId', function (req, res) {
 });
 
 app.get('/v1/playlists', getPlaylistsV1);
+
+app.get('/v1/playlists/:playlist', getPlaylistV1);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
