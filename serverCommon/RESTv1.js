eddb1214da3764cee4fd2660aa26ba3804cebf2c
ownimage <keith@ownimage.co.uk>
2018-03-26 17:01:06 +0100

    tidied up module exports for RESTv1

--- serverCommon/RESTv1.js
+++ serverCommon/RESTv1.js
@@ -1,66 +1,70 @@
 const fs = require('fs');
 const path = require('path');
-const neopixelDriverV1 =  require('./NeoPixelDriverV1');
+const neopixelDriverV1 = require('./NeoPixelDriverV1');
 
 const config = require("./config").getConfig();
 
+const getImagesV1 = (req, res) => {
+    console.log("getImagesV1");
+    fs.readdir(config.imagesFolder, (err, files) => {
+        console.log(`getImagesV1 ` + JSON.stringify(files));
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(files);
+    });
+};
+
+const getPlaylistsV1 = (req, res) => {
+    console.log("getPlaylistsV1");
+    fs.readdir(config.playlistFolder, (err, files) => {
+        console.log(`getPlaylistsV1 ` + JSON.stringify(files));
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(files);
+    });
+};
+
+const getPlaylistV1 = (req, res) => {
+    console.log("getPlaylistV1");
+    console.log(req.params.playlist);
+    const playlist = req.params.playlist;
+    const filePath = path.join(config.playlistFolder, playlist);
+    fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
+        if (!err) {
+            console.log('received data: ' + data);
+            res.header("Access-Control-Allow-Origin", "*");
+            res.send(data);
+        } else {
+            console.log(err);
+        }
+    });
+};
+
+const postPlaylistV1 = (req, res) => {
+    console.log("postPlaylistV1");
+    console.log(req.params.playlist);
+    console.log("req.body =" + JSON.stringify(req.body));
+    const playlist = req.params.playlist;
+    const filePath = path.join(config.playlistFolder, playlist);
+    fs.writeFile(filePath, JSON.stringify(req.body, null, 2), function (err) {
+        if (!err) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Content-Type", "text/plain");
+            res.end();
+        } else {
+            res.sendStatus(500);
+        }
+    });
+};
+
+const playV1 = (req, res) => {
+    console.log("playV1");
+    console.log("req.body =" + JSON.stringify(req.body));
+    neopixelDriverV1.playPlaylist(req.body);
+};
+
 module.exports = {
-
-    getImagesV1: (req, res) => {
-        console.log("getImagesV1");
-        fs.readdir(config.imagesFolder, (err, files) => {
-            console.log(`getImagesV1 ` + JSON.stringify(files));
-            res.header("Access-Control-Allow-Origin", "*");
-            res.send(files);
-        });
-    },
-
-    getPlaylistsV1: (req, res) => {
-        console.log("getPlaylistsV1");
-        fs.readdir(config.playlistFolder, (err, files) => {
-            console.log(`getPlaylistsV1 ` + JSON.stringify(files));
-            res.header("Access-Control-Allow-Origin", "*");
-            res.send(files);
-        });
-    },
-
-    getPlaylistV1: (req, res) => {
-        console.log("getPlaylistV1");
-        console.log(req.params.playlist);
-        const playlist = req.params.playlist;
-        const filePath = path.join(config.playlistFolder, playlist);
-        fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
-            if (!err) {
-                console.log('received data: ' + data);
-                res.header("Access-Control-Allow-Origin", "*");
-                res.send(data);
-            } else {
-                console.log(err);
-            }
-        });
-    },
-
-    postPlaylistV1: (req, res) => {
-        console.log("postPlaylistV1");
-        console.log(req.params.playlist);
-        console.log("req.body =" + JSON.stringify(req.body));
-        const playlist = req.params.playlist;
-        const filePath = path.join(config.playlistFolder, playlist);
-        fs.writeFile(filePath, JSON.stringify(req.body, null, 2), function (err) {
-            if (!err) {
-                res.header("Access-Control-Allow-Origin", "*");
-                res.header("Content-Type", "text/plain");
-                res.end();
-            } else {
-                res.sendStatus(500);
-            }
-        });
-    },
-
-    playV1: (req, res) => {
-        console.log("playV1");
-        console.log("req.body =" + JSON.stringify(req.body));
-        neopixelDriverV1.playPlaylist(req.body);
-    }
-
-}
+    getImagesV1,
+    getPlaylistsV1,
+    getPlaylistV1,
+    postPlaylistV1,
+    playV1
+};
