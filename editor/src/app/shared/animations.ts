3f0bb6c987dfc10655f58e8a443fb92fce843a2c
ownimage <keith@ownimage.co.uk>
2018-04-05 10:01:14 +0100

    fixed end state

--- editor/src/app/shared/animations.ts
+++ editor/src/app/shared/animations.ts
@@ -5,13 +5,15 @@ import {trigger, state, animate, transition, style, query} from '@angular/animat
 export const slideInOutAnimation =
     // trigger name for attaching this animation to an element using the [@triggerName] syntax
     trigger('slideInOutAnimation', [
-        state('void', style({position:'fixed', width:'100%'}) ),
-        state('*', style({position:'fixed', width:'100%'}) ),
+        state('void', style({position:'static', width:'100%'}) ),
+        state('*', style({position:'static', width:'100%'}) ),
         transition(':enter', [  // before 2.1: transition('void => *', [
+            style({position:'fixed', width:'100%'}),
             style({transform: 'translateY(100%)'}),
             animate('0.5s ease-in-out', style({transform: 'translateY(0%)'}))
         ]),
         transition(':leave', [  // before 2.1: transition('* => void', [
+            style({position:'fixed', width:'100%'}),
             style({transform: 'translateY(0%)'}),
             animate('0.5s ease-in-out', style({transform: 'translateY(-100%)'}))
         ])
