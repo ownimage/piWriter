57380e1fae51f23d7693c8f34f278d087d747674
ownimage <keith@ownimage.co.uk>
2018-05-04 15:38:37 +0100

    release 2.3 changes

--- startServer.sh
+++ startServer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-pid=`ps -auxww | grep node | grep -v grep | grep -v sudo | awk '{print $2}'`
+pid=`ps -auxww | grep node | grep -v grep | grep -v sudo | grep -v PATH | awk '{print $2}'`
 if [ 0$pid -eq 0 ]
 then 
     echo server starting > /home/pi/crontab.log
     cd /home/pi/piWriter/serverRPi
-    sudo /home/pi/.nvm/versions/node/v9.9.0/bin/node server
+    sudo sh -c 'PATH=$PATH:/home/pi/.nvm/versions/node/v9.9.0/bin/; ts-node server'
 fi
