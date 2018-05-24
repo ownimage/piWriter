0c729d03f67d72bb00b7349f8e8b1f6e4f461ddd
ownimage <keith@ownimage.co.uk>
2018-05-24 12:16:25 +0100

    Issue #148
    serverInfoDTO in editor should be reused in server (removed constants from testing too)

--- serverCommon/src/dto/serverInfoDTO.model.ts
+++ serverCommon/src/dto/serverInfoDTO.model.ts
@@ -1,7 +1,7 @@
 export class ServerInfoDTO {
     constructor(public uptime: { d, h, m, s },
                 public totalmem: number,
-                public freemem: 12001468416,
+                public freemem: number,
                 public loadavg: [number, number, number],
                 public temp: number
     ) {
